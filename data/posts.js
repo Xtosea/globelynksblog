48cef62d7dd9b9a4a3c15b3a4eaf6af8e464f027
Chris <106016563+Xtosea@users.noreply.github.com>
2026-01-28 23:00:41 +0100

    Create posts.js

---
+++ data/posts.js
@@ -0,0 +1,20 @@
+export const posts = [
+  {
+    title: 'My First Blog Post',
+    slug: 'my-first-blog-post',
+    date: '2026-01-28',
+    excerpt: 'This is a fresh blog post without Sanity.',
+    coverImage: '/images/cover1.jpg',
+    author: 'Chris Ikelegbe',
+    content: 'Hello world! This is my first static blog post.',
+  },
+  {
+    title: 'Another Post',
+    slug: 'another-post',
+    date: '2026-01-28',
+    excerpt: 'Another static post example.',
+    coverImage: '/images/cover2.jpg',
+    author: 'Chris Ikelegbe',
+    content: 'More content here...',
+  },
+]
