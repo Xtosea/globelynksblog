2169e4ec32cae1ece61ae1091865a3a72a97c2ab
Chris <106016563+Xtosea@users.noreply.github.com>
2026-01-29 01:22:28 +0100

    Add Footer component with copyright notice

---
+++ components/footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="border-t mt-16 py-8 text-center text-sm text-gray-500">
+      © {new Date().getFullYear()} Globelynks. All rights reserved.
+    </footer>
+  )
+}
