82ff40927c88c1fe6a1df57f1e148c5395f386a1
Chris <106016563+Xtosea@users.noreply.github.com>
2026-01-29 01:24:27 +0100

    Enhance App component with SEO meta tags
    Added Open Graph and Twitter meta tags for better SEO.

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,17 @@
-import '../styles/globals.css'
+import Head from "next/head"
+import "../styles/globals.css"
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+  return (
+    <>
+      <Head>
+        <meta property="og:type" content="article" />
+        <meta property="og:site_name" content="Globelynks Blog" />
+        <meta property="og:image" content="/og.png" />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  )
+    }
