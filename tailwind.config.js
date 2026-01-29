78a32a136a2427b6811ec7454cece304f699472d
Chris <106016563+Xtosea@users.noreply.github.com>
2026-01-29 13:13:44 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
-// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
 const typography = require('@tailwindcss/typography');
 
 module.exports = {
   content: [
-    "./src/**/*.{js,ts,jsx,tsx,html}", // adjust to your project structure
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {
