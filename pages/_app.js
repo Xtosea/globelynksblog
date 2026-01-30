0f0f574bb1481d4650e1ffe9e7296e90b15325ac
Chris <106016563+Xtosea@users.noreply.github.com>
2026-01-30 14:04:57 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,36 @@
-import Head from "next/head";
+import Head from "next/head"
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
+        {/* Tailwind CDN */}
+        <script src="https://cdn.tailwindcss.com"></script>
+
+        {/* Tailwind Config */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              tailwind.config = {
+                theme: {
+                  extend: {
+                    colors: {
+                      primary: '#2276fc'
+                    }
+                  }
+                }
+              }
+            `,
+          }}
+        />
+
+        {/* SEO */}
         <meta property="og:type" content="article" />
         <meta property="og:site_name" content="Globelynks Blog" />
-        <meta property="og:image" content="/og.png" />
         <meta name="twitter:card" content="summary_large_image" />
       </Head>
 
       <Component {...pageProps} />
     </>
-  );
+  )
 }
