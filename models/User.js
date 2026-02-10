d6a0ad3accb5091dfad1a85193fb58c5e400d37f
Chris <106016563+Xtosea@users.noreply.github.com>
2026-02-10 11:38:11 +0100

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,5 +1,4 @@
 import mongoose from "mongoose";
-import bcrypt from "bcryptjs";
 
 const UserSchema = new mongoose.Schema({
   name: { type: String, required: true },
@@ -7,20 +6,7 @@ const UserSchema = new mongoose.Schema({
   password: { type: String, required: true },
   role: { type: String, default: "admin" },
   createdAt: { type: Date, default: Date.now },
-})
-
-
-// Hash password before saving
-UserSchema.pre("save", async function(next) {
-  if (!this.isModified("password")) return next();
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
-  next();
 });
 
-export default mongoose.models.User || mongoose.model("User", UserSchema);
-
-
-
-
-  
+export default mongoose.models.User ||
+  mongoose.model("User", UserSchema);
