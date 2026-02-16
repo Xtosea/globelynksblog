372c098caac0b20c963fffc35eeeb38a4afbf3a3
Chris <106016563+Xtosea@users.noreply.github.com>
2026-02-16 15:42:54 +0100

    Update posts.js

--- pages/admin/posts.js
+++ pages/admin/posts.js
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { useRouter } from "next/navigation";
+import { useRouter } from "next/router";
 
 export default function ManagePosts() {
   const [posts, setPosts] = useState([]);
