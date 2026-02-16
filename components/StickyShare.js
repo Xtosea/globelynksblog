2a6cf64ef0dc0b0f0f391687f8a56eec54ece72b
Chris <106016563+Xtosea@users.noreply.github.com>
2026-02-16 09:50:03 +0100

    Create StickyShare.js

---
+++ components/StickyShare.js
@@ -0,0 +1,9 @@
+export default function StickyShare() {
+  return (
+    <div className="fixed left-2 top-1/3 flex flex-col gap-3 z-50">
+      <a href="#" className="bg-blue-600 text-white px-3 py-2 rounded">F</a>
+      <a href="#" className="bg-sky-500 text-white px-3 py-2 rounded">T</a>
+      <a href="#" className="bg-green-600 text-white px-3 py-2 rounded">W</a>
+    </div>
+  )
+}
