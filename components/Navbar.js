418a38fe4242e1d7ce3a91c9d8b96bfabb32d145
Chris <106016563+Xtosea@users.noreply.github.com>
2026-02-16 11:52:38 +0100

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,7 +2,7 @@
 
 import { useState } from "react"
 import Link from "next/link"
-import { useRouter } from "next/router"
+
 import { usePathname } from "next/navigation"
 
 export default function Navbar() {
