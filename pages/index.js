073148ad31ae1d5dcad230c5a9a22acb38808632
Chris <106016563+Xtosea@users.noreply.github.com>
2026-02-16 09:13:56 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,9 @@
 
 import { useEffect, useState } from "react"
 import Link from "next/link"
-import Navbar from "./components/Navbar"
+import Navbar from "../components/Navbar"
+import layout from ../app/layout"
+
 
 export default function Home() {
   const [posts, setPosts] = useState([])
