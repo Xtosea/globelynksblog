e002cce7a5380fc90a5004351ee0acabd6da1abc
Chris <106016563+Xtosea@users.noreply.github.com>
2026-02-16 18:52:05 +0100

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="border-t mt-16 py-8 text-center text-sm text-gray-500">
-      © {new Date().getFullYear()} Powered by Board Givers International Multipurpose Limited. All rights reserved.
+      © {new Date().getFullYear()} Powered by Board Givers International Multipurpose Limited. All rights reserved
     </footer>
   )
 }
