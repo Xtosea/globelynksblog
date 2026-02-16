28c7e47211980461aad77eaa56cf4edc8007c1a2
Chris <106016563+Xtosea@users.noreply.github.com>
2026-02-16 13:25:36 +0100

    Update StickyShare.js

--- components/StickyShare.js
+++ components/StickyShare.js
@@ -2,10 +2,10 @@
 
 export default function StickyShare() {
   return (
-    <div className="fixed left-2 top-1/3 flex flex-col gap-3 z-50">
-      <a href="#" className="bg-blue-600 text-white px-3 py-2 rounded">F</a>
-      <a href="#" className="bg-sky-500 text-white px-3 py-2 rounded">T</a>
-      <a href="#" className="bg-green-600 text-white px-3 py-2 rounded">W</a>
+    <div className="fixed left-2 top-1/3 flex flex-col gap-3 z-[200] pointer-events-auto">
+      <a href="#" className="bg-blue-600 text-white px-3 py-2 rounded hover:bg-blue-700 transition">F</a>
+      <a href="#" className="bg-sky-500 text-white px-3 py-2 rounded hover:bg-sky-600 transition">T</a>
+      <a href="#" className="bg-green-600 text-white px-3 py-2 rounded hover:bg-green-700 transition">W</a>
     </div>
   )
 }
