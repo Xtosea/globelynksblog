53f855682caa254abfd9b037307e0b64c96eace3
Chris <106016563+Xtosea@users.noreply.github.com>
2026-02-16 09:14:45 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@
 import { useEffect, useState } from "react"
 import Link from "next/link"
 import Navbar from "../components/Navbar"
-import layout from ../app/layout"
+import layout from "../app/layout"
 
 
 export default function Home() {
