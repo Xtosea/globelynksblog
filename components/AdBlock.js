55d3ba293c3a10de476bf41244717828e5f02a69
Chris <106016563+Xtosea@users.noreply.github.com>
2026-02-16 09:49:28 +0100

    Create AdBlock.js

---
+++ components/AdBlock.js
@@ -0,0 +1,10 @@
+export default function AdBlock() {
+  return (
+    <div className="bg-gray-200 dark:bg-gray-700 text-center p-6 rounded my-6">
+      <p className="text-sm font-semibold">ADVERTISEMENT</p>
+      <div className="h-32 flex items-center justify-center">
+        Your Ad Here
+      </div>
+    </div>
+  )
+}
