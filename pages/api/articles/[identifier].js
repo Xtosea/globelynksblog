4f63f8d8242afd711ed9c85172807f906f94ebf9
Chris <106016563+Xtosea@users.noreply.github.com>
2026-02-18 00:01:58 +0100

    Create [identifier].js

---
+++ pages/api/articles/[identifier].js
@@ -0,0 +1,26 @@
+import dbConnect from "@/lib/mongodb";
+import Article from "@/models/Article";
+
+export default async function handler(req, res) {
+  const { identifier } = req.query;
+
+  await dbConnect();
+
+  let article = null;
+
+  // If it's MongoDB ObjectId
+  if (/^[0-9a-fA-F]{24}$/.test(identifier)) {
+    article = await Article.findById(identifier);
+  }
+
+  // If not found, try slug
+  if (!article) {
+    article = await Article.findOne({ slug: identifier });
+  }
+
+  if (!article) {
+    return res.status(404).json({ message: "Not found" });
+  }
+
+  res.status(200).json(article);
+}
