cbe9d80b02a16ba300633ce8d382d3d23bcfec51
Chris <106016563+Xtosea@users.noreply.github.com>
2026-02-17 19:54:39 +0100

    Update [slug].js

--- pages/api/posts/[slug].js
+++ pages/api/posts/[slug].js
@@ -1,4 +1,4 @@
-import { connectDB } from "../../../lib/mongodb"
+import { connectDB } from '@/lib/mongodb';
 import Post from "../../../models/Post"
 
 export default async function handler(req, res) {
