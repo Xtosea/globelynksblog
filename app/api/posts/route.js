6aba3c9c2e7f66dc7320c58a75aa2c8086a24a93
Chris <106016563+Xtosea@users.noreply.github.com>
2026-02-17 19:51:33 +0100

    Update route.js

--- app/api/posts/route.js
+++ app/api/posts/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { connectDB } from "../../../lib/mongodb";
+import { connectDB } from '@/lib/mongodb';
 import Post from "../../../models/Post";
 
 export async function GET() {
