e15d62ce6e24849f03fbcb7581d0e6f546ddf6dc
Chris <106016563+Xtosea@users.noreply.github.com>
2026-02-17 19:09:33 +0100

    Create Article.js

---
+++ models/Article.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const ArticleSchema = new mongoose.Schema({
+  title: { type: String, required: true, unique: true },
+  content: { type: String, required: true },
+  source: { type: String, required: true },
+  category: { type: String, default: "General" },
+  tags: { type: [String], default: [] },
+  scheduledDate: { type: Date, default: Date.now },
+  published: { type: Boolean, default: false },
+  views: { type: Number, default: 0 },
+  createdAt: { type: Date, default: Date.now },
+});
+
+export default mongoose.models.Article || mongoose.model("Article", ArticleSchema);
