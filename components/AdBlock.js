4f82bad8755e36a36d565c7e9316e01b9036f826
Chris <106016563+Xtosea@users.noreply.github.com>
2026-02-19 20:16:22 +0100

    Update AdBlock.js

--- components/AdBlock.js
+++ components/AdBlock.js
@@ -1,10 +1,13 @@
-export default function AdBlock() {
+import TrendingAd from "@/components/ads/TrendingAd";
+
+export default function Home() {
   return (
-    <div className="bg-gray-200 dark:bg-gray-700 text-center p-6 rounded my-6">
-      <p className="text-sm font-semibold">ADVERTISEMENT</p>
-      <div className="h-32 flex items-center justify-center">
-        Your Ad Here
-      </div>
+    <div>
+      <h1>Trending News</h1>
+
+      <TrendingAd />
+
+      {/* Your posts */}
     </div>
-  )
+  );
 }
